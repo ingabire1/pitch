20d4b55c8c2e228e2e413a3fc2c29ab24afd17e4
JURU <jurassu10@gmail.com>
2019-02-18 12:26:54 +0200

    add os.environ.get() function to get NEWS_API_KEY and SECRET_KEY

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
